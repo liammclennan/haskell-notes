5d2634eb0f31642036d720eafa3fab2014a354be
Liam McLennan <liam.mclennan@gmail.com>
2012-10-10 21:19:48 +1000

    pattern matching. Record syntax.

--- functions.hs
+++ functions.hs
@@ -1,1 +1,9 @@
 add f s t = f + s + t
+
+firstFirst :: (Num a) => [(a,a)] -> a
+firstFirst ((f,s):xs) = f
+
+data Person = Person {
+    age       :: Int,
+    name      :: String
+  } deriving (Show)
