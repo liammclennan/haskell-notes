20ecac569d82dac94a431695d2acae4a64402244
Liam McLennan <liam.mclennan@gmail.com>
2012-11-05 21:07:12 +1000

    refactored problem 2

--- projecteuler/p2.hs
+++ projecteuler/p2.hs
@@ -1,10 +1,5 @@
-
--- sum fibs < 4000000
---fibGen [] = [1]
---fibGen [1] = [1,2]
---fibGen (x:x1:xs) = x+x1:x:x1:xs
-
-
+-- By considering the terms in the Fibonacci sequence whose values do not 
+-- exceed four million, find the sum of the even-valued terms.
 fibsLessThan n (x:xs) 
     | x < n = fibsLessThan n (fibGen (x:xs))
     | otherwise = (sum . (filter isEven)) xs
