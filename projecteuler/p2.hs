52da880dce9679e2e44b9632e2871593fe878e60
Liam McLennan <liam.mclennan@gmail.com>
2012-11-05 21:06:04 +1000

    refactored problem 2

--- projecteuler/p2.hs
+++ projecteuler/p2.hs
@@ -1,10 +1,14 @@
 
 -- sum fibs < 4000000
-fibGen [] = [1]
-fibGen [1] = [1,2]
-fibGen (x:x1:xs) = x+x1:x:x1:xs
+--fibGen [] = [1]
+--fibGen [1] = [1,2]
+--fibGen (x:x1:xs) = x+x1:x:x1:xs
+
 
 fibsLessThan n (x:xs) 
     | x < n = fibsLessThan n (fibGen (x:xs))
-    | otherwise = sum  (filter isEven xs)
+    | otherwise = (sum . (filter isEven)) xs
      where isEven i = i `mod` 2 == 0
+           fibGen [] = [1]
+           fibGen [1] = [1,2]
+           fibGen (x:x1:xs) = x+x1:x:x1:xs
